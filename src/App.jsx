184e4c61115d986a00701702520566896149af64
LucasMarchena <lucasmarchena1999@gmail.com>
2024-10-08 17:55:32 -0300

    clase-3/jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Card1 } from "./components/Card1";
 import { Navbar } from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Card1 />
     </div>
   );
 }
