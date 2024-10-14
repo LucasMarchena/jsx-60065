85aa7ab3ddb6ef5d3826e7131b07b336054eaa33
LucasMarchena <lucasmarchena1999@gmail.com>
2024-10-14 13:57:34 -0300

    clase-5/componentes2

---
+++ src/components/products.js
@@ -0,0 +1,63 @@
+export const products = [
+  {
+    id: "1",
+    title: "Nike Dunk Low Retro Premium",
+    price: 209.9,
+    stock: 5,
+    imageUrl:
+      "https://res.cloudinary.com/ddtbb44ju/image/upload/v1728594819/NIKE_DUNK_LOW_RETRO_PRM_acwaag.jpg",
+    description: "Nike Dunk Low Retro Premium",
+    category: "Urbano",
+  },
+  {
+    id: "2",
+    title: "Nike Ai Force 1 x Supreme",
+    price: 239.9,
+    stock: 5,
+    imageUrl:
+      "https://res.cloudinary.com/ddtbb44ju/image/upload/v1728594977/FV3702-121_0_blzc5u.jpg",
+    description: "Nike Ai Force 1 x Supreme",
+    category: "Urbano",
+  },
+  {
+    id: "3",
+    title: "Puma Future 7 Match",
+    price: 159.9,
+    stock: 5,
+    imageUrl:
+      "https://res.cloudinary.com/ddtbb44ju/image/upload/v1728595066/10826501_0_e9vder.jpg",
+    description: "Puma Future 7 Match",
+    category: "Deportivo",
+  },
+  {
+    id: "4",
+    title: "Adidas F50 Elites",
+    price: 209.9,
+    stock: 5,
+    imageUrl:
+      "https://res.cloudinary.com/ddtbb44ju/image/upload/v1728596820/IF8818_0_nhrmqe.jpg",
+    description: "Adidas F50 Elites",
+    category: "Urbano",
+  },
+  {
+    id: "5",
+    title: "3206 (marrón)",
+    price: 119.9,
+    stock: 5,
+    imageUrl:
+      "https://res.cloudinary.com/ddtbb44ju/image/upload/v1728595365/3206_011-2547a35fcb43f25a1a15881592290434-1024-1024_dtodsa.jpg",
+    description: "Zapato de vestir marrón con suela de goma",
+    category: "Elegante",
+  },
+  {
+    id: "6",
+    title: "Gon (Negro)",
+    price: 209.9,
+    stock: 5,
+    imageUrl:
+      "https://acdn.mitiendanube.com/stores/598/845/products/8060_011-30adc105e2d000c3f815198697144335-640-0.jpg",
+    description:
+      "Zapato confeccionado en cuero vacuno trabajado de suave textura con brillo.",
+    category: "Elegante",
+  },
+];
